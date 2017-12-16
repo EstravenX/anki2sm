44cd933277ca156467291fc44014d6c787e415a1
cutie <cutie@users.noreply.github.com>
2017-12-16 17:34:11 +0100

    added video, generates correct path for import

--- anki2sm.py
+++ anki2sm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+import re
 import shutil
 import click
 import magic
@@ -11,6 +13,8 @@ from pathlib import Path
 
 from yattag import Doc
 
+TMP = "out/out_files/elements"
+
 @click.command()
 @click.option('--file', help='Filename.')
 @click.option('--v', default=True, help='Filename.')
@@ -21,16 +25,20 @@ def hello(file, v):
 
   p = unzip_file(Path(file))
   media = unpack_media(p)
-  doc = unpack_db(p)
   out = Path("out")
   out.mkdir(parents=True, exist_ok=True)
-  with open("out/out.xml", "w") as f:
-    f.write(doc.getvalue())
+  elements = Path("{}/out_files/elements".format(out.as_posix()))
+  elements.mkdir(parents=True, exist_ok=True)
   for k in media:
-    shutil.move(p.joinpath(k).as_posix(), out.joinpath(media[k]).as_posix())
+    shutil.move(p.joinpath(k).as_posix(), elements.joinpath(media[k]).as_posix())
+
+  doc = unpack_db(p)
+
+  with open("{}/out.xml".format(TMP), "w") as f:
+    f.write(doc.getvalue())
   return 0
 
-
+# @Todo: use temporary dir
 def unzip_file(zipfile_path: Path) -> Path:
   if "zip" not in magic.from_file(zipfile_path.as_posix(), mime=True):
     print("apkg does not appear to be a ZIP file...")
@@ -57,7 +65,7 @@ def unpack_db(path: Path):
   sep = "\x1f" #some kind of control code that is not valid XML
   get_id = get_id_func()
 
-  # @Todo, each collection should have its own concept
+
 
   with tag('SuperMemoCollection'):
     with tag('Count'):
@@ -69,29 +77,67 @@ def unpack_db(path: Path):
         text('Test')
       with tag('Type'): #Concept, Topic or Item
         text('Topic')
-      for row in cursor.fetchall():
+      for row in cursor.fetchall(): # @Todo, each collection should have its own concept or topic
         id, guid, mid, mod, usn, tags, flds, sfld, csum, flags, data = row
         qs = flds.split(sep)
-        d = pq(flds+sfld)
-        img = d("img").attr("src")
+        e = ""
+        if isinstance(flds, str):
+          e += flds
+        if isinstance(flds, str):
+          sfld += sfld
+        d = pq(e)
+        img = d("img").attr("src") or "" #@Todo: multiple images
         img_path = ""
         if img:
           img_path = "[SecondaryStorage]\\{}".format(img)
+        #anki supports (claimed)
+        #jpg png gif tiff svg tif jpeg mp3 ogg wav avi ogv
+        #mpg mpeg mov mp4 mkv ogx ogv oga flv swf flac
+        #sm17 supports (known)
+        #jpg png gif bmp jpeg mp3 avi mp4
+        content_sound = ()
+        content_video = ()
+        if "[sound:" in e: #@Todo: what happens if [ or ] is in the name?
+          g = re.search("\[sound\:([^\]]+)", e)
+          for p in g.groups():
+            m = Path("{}/{}".format(TMP,p))
+            if m.exists():
+              if any([ext in m.suffix for ext in ["mp3", "ogg", "wav"]]) \
+                  or "audio" in magic.from_file(m.as_posix(), mime=True):
+                content_sound = content_sound + (p,)
+              if any([ext in m.suffix for ext in ["mp4", "wmv", "mkv"]]) \
+                  or "video" in magic.from_file(m.as_posix(), mime=True):
+                content_video = content_video + (p,)
         with tag('SuperMemoElement'):
           with tag('ID'):
             text(get_id())
           with tag('Type'):
             text('Item')
-          with tag('Content'):
+          with tag('Content'): #zero or more of Question Answer Sound Video Image Binary
             with tag('Question'):
               text(strip_control_characters(qs[0]))
             with tag('Answer'):
               text(strip_control_characters(qs[1]))
-            with tag('Image'):
-              with tag('URL'):
-                text(img_path)
-              with tag('Name'):
-                text(img)
+            if img:
+              with tag('Image'):
+                with tag('URL'):
+                  text(img_path)
+                with tag('Name'):
+                  text(img)
+            for s in content_video:
+              with tag('Video'):
+                with tag('URL'):
+                  text("[SecondaryStorage]\\{}".format(s))
+                with tag('Name'):
+                  text(s)
+            for s in content_sound:
+              with tag('Sound'):
+                with tag('URL'):
+                  text("[SecondaryStorage]\\{}".format(s))
+                with tag('Name'):
+                  text(s)
+                with tag('Text'):
+                  text("")
           with tag("LearningData"): #@Todo, convert anki learning data to sm
             with tag("Interval"):
               text("1")
@@ -107,8 +153,7 @@ def unpack_db(path: Path):
               text("")
             with tag("RepHist"):
               text("")
-
-
+  pp("Amount of cards: {}".format(int(get_id())-1))
   return doc
 
 def unpack_media(media_dir: Path):
